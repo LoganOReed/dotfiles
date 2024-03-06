aaa2733457d2cbac2b89596e3223d318c0472322
Logan Reed <me@loganreed.org>
2024-03-06 00:47:50 -0500

    hyprland

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -21,7 +21,6 @@
     # You can also split up your configuration and import pieces of it here:
     # ./users.nix
     ./home-manager.nix
-    ./xfce.nix
 
     # Import your generated (nixos-generate-config) hardware configuration
     ./hardware-configuration.nix
