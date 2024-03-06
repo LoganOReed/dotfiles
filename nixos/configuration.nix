c3bc6af9886612488f1e57d4e11c80edd88465f5
Logan Reed <me@loganreed.org>
2024-03-06 02:08:23 -0500

    changed zsh prompt

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -180,6 +180,7 @@
 	  noto-fonts
 	  (nerdfonts.override { fonts = [ "FiraCode" ]; })
       ];
+  };
 
   # This setups a SSH server. Very important if you're setting up a headless system.
   # Feel free to remove if you don't need it.
