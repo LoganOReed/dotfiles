5a572eb8ceda76491ad13d562d7e9c9d90dc18ae
Logan Reed <me@loganreed.org>
2024-03-06 00:04:04 -0500

    hyprland

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -12,14 +12,13 @@
   imports = [
     # If you want to use modules your own flake exports (from modules/home-manager):
     # outputs.homeManagerModules.example
-    outputs.homeManagerModules.hyprland
 
     # Or modules exported from other flakes (such as nix-colors):
     # inputs.nix-colors.homeManagerModules.default
 
     # You can also split up your configuration and import pieces of it here:
     # ./nvim.nix
-  ];
+  ] ++ (builtins.attrValues outputs.homeManagerModules;
 
   nixpkgs = {
     # You can add overlays here
