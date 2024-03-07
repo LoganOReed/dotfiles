02f5932918fc92925f9664b1006099b10efd11f4
Logan Reed <me@loganreed.org>
2024-03-07 11:23:34 -0500

    finally fixed swaylock

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -18,7 +18,6 @@ let
     name = "dbus-sway-environment";
     destination = "/bin/dbus-sway-environment";
     executable = true;
-
     text = ''
   dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
   systemctl --user stop pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
