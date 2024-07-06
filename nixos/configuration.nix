5bdd891b10d8294c17267295042fefdaf1722363
Logan Reed <me@loganreed.org>
2024-07-06 16:01:58 -0400

    Added Env Vars for XDG

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -85,6 +85,32 @@
 
   # FIXME: Add the rest of your current configuration
 
+
+# Set environment variables
+# NOTE: I'm setting this so ~/Downloads stops showing up
+    environment.variables = {
+        NIXOS_CONFIG = "$HOME/dotfiles/nixos/configuration.nix";
+        NIXOS_CONFIG_DIR = "$HOME/dotfiles";
+        XDG_STATE_HOME = "$HOME/.local/state";
+        XDG_CONFIG_HOME = "$HOME/.config";
+        XDG_DATA_HOME = "$HOME/.local/share";
+        XDG_CACHE_HOME = "$HOME/.cache";
+        GTK_RC_FILES = "$HOME/.local/share/gtk-1.0/gtkrc";
+        GTK2_RC_FILES = "$HOME/.local/share/gtk-2.0/gtkrc";
+        MOZ_ENABLE_WAYLAND = "1";
+        DISABLE_QT5_COMPAT = "0";
+        DIRENV_LOG_FORMAT = "";
+
+        XDG_DESKTOP_DIR="$HOME/misc";
+        XDG_PUBLICSHARE_DIR="$HOME/misc";
+        XDG_TEMPLATES_DIR="$HOME/misc";
+        XDG_DOWNLOAD_DIR="$HOME/downloads";
+        XDG_DOCUMENTS_DIR="$HOME/documents";
+        XDG_MUSIC_DIR="$HOME/documents/music";
+        XDG_PICTURES_DIR="$HOME/documents/pictures";
+        XDG_VIDEOS_DIR="$HOME/documents/videos";
+    };
+
   environment.sessionVariables = {
     FLAKE = "/home/occam/dotfiles";
     EDITOR = "nvim";
