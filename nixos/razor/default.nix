34e0d353cc9a1512db5cff8616e39ee1bd16348c
LoganOReed <me@loganreed.org>
2024-12-31 16:31:48 -0500

    Added snippets and bat saving for razor

--- nixos/razor/default.nix
+++ nixos/razor/default.nix
@@ -364,6 +364,9 @@
     Defaults        timestamp_timeout=60
   '';
 
+  services.tlp.enable = true;
+  services.thermald.enable = true;
+
   # This setups a SSH server. Very important if you're setting up a headless system.
   # Feel free to remove if you don't need it.
   services.openssh = {
