1c55670506c01de9d73e4ca4e104dec802f518a8
LoganOReed <me@loganreed.org>
2025-01-03 20:00:52 -0500

    Added yubikey pam

--- nixos/blade/default.nix
+++ nixos/blade/default.nix
@@ -214,7 +214,28 @@
   # Setup sway
   security.polkit.enable = true;
   security.pam.services.swaylock = {
-    text = "auth include login";
+    u2fAuth = true;
+    # text = "auth include login";
+  };
+
+security.pam.services = {
+  login.u2fAuth = true;
+  sudo.u2fAuth = true;
+};
+
+  # Yubikey stuff
+  security.pam.u2f = {
+    enable = true;
+    interactive = true;
+    cue = true;
+  };
+
+  security.pam.u2f = {
+    origin = "pam://yubi";
+    authFile = pkgs.writeText "u2f-mappings" (lib.concatStrings [
+      "occam"
+      ":DTudv4m1IOG1f73ackmJtYmiAxyN223GQqHHfn8Oota9+iKE1q8ItmFB3EiyP645hwZwdGrp9Lo3laxIN20nAA==,s8BBGUCEtcspC0uI6u9wgCltpvkqRN/wO1KuAq6gYGlCtigfEoQBrkr9fcXvwZds8QWGcbFzeRkF9LHybqUC4Q==,es256,+presence"
+    ]);
   };
 
   xdg.portal = {
